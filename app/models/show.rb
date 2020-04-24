bcd64f55e4afcd7d7d425a031fb528185effd556
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-24 19:32:33 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
+  has_many :characters 
+  has_many :actors, through: :characters
+  belongs_to :network
+  
+  def actors_list 
+    #has an #actors_list method that returns a list of the full names of each actor associated with the show
+    self.actors.map do |actor|
+      "#{actor.first_name} #{actor.last_name}"
+    end 
+    
+    
+  end 
+  
   
 end
